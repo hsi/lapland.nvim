faa9c53f18529e12be9a7d51e10085ed56870592
hsi <53867065+hsi@users.noreply.github.com>
2025-08-26 23:49:51 +0200

    Align graphical fruit colors to terminal ones

--- lua/lapland/colors.lua
+++ lua/lapland/colors.lua
@@ -1,6 +1,6 @@
 local colors = {
-    daddy_o        = { terminal = 144, graphical = '#afaf87' },
-    fancy_fuchsia  = { terminal = 198, graphical = '#ff0087' },
+    daddy_o        = { terminal = 144, graphical = '#b0b088' },
+    fancy_fuchsia  = { terminal = 198, graphical = '#ff0088' },
     gray1          = { terminal = 232, graphical = '#080808' },
     gray2          = { terminal = 233, graphical = '#121212' },
     gray3          = { terminal = 234, graphical = '#1c1c1c' },
@@ -8,12 +8,11 @@ local colors = {
     gray9          = { terminal = 240, graphical = '#585858' },
     gray13         = { terminal = 244, graphical = '#808080' },
     gray22         = { terminal = 253, graphical = '#dadada' },
-    green          = { terminal =  46, graphical = '#00ff00' },
-    hobgoblin      = { terminal =  36, graphical = '#00af87' },
-    ice_cold_stare = { terminal = 153, graphical = '#afd7ff' },
-    krishna_blue   = { terminal =  39, graphical = '#00afff' },
+    hobgoblin      = { terminal =  36, graphical = '#00b088' },
+    ice_cold_stare = { terminal = 153, graphical = '#b0d8ff' },  -- Light blueberry
+    krishna_blue   = { terminal =  39, graphical = '#00b0ff' },  -- Blueberry
     purple         = { terminal =  60, graphical = '#5f5f88' },  -- Bilberry
-    vic_20_creme   = { terminal = 229, graphical = '#ffffaf' },
+    vic_20_creme   = { terminal = 229, graphical = '#ffffb0' },
     mandarin_jelly = { terminal = 208, graphical = '#ff8800' },  -- Cloudberry
     white          = { terminal = 231, graphical = '#ffffff' },
 }
